4807ce3204d6c42593d5af6676d6c895fe7fc782
momapu <you@example.com>
2025-12-23 21:10:58 +0300

    circle

--- index.php
+++ index.php
@@ -49,6 +49,22 @@
                 echo "Username: {$username}<br>";
             }
         ?>
+        <form action="index.php" method="post">
+            <label>Radius:</label><br>
+            <input type="text" name="radius"><br>
+            <input type="submit" value="Calculate">
+        </form>
+        <?php
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                $radius = $_POST['radius'];
+                $circumference = round(2 * pi() * $radius, 2);
+                $area = round(pi() * pow($radius, 2), 2);
+                $volume = round((4/3) * pi() * pow($radius, 3), 2);
+                echo "Circumference: {$circumference} cm<br>";
+                echo "Area: {$area} cm²<br>";
+                echo "Volume: {$volume} cm³<br>";
+            }
+        ?>
 
     </body>
 </html>

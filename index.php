4083d86222d186f795e646cde725e92ee6337b94
momapu <you@example.com>
2025-12-23 21:16:01 +0300

    switch

--- index.php
+++ index.php
@@ -49,6 +49,29 @@
         echo "The weather is bad.";
     }
 ?>
+<?php
+$grade = 'B';
+switch ($grade) {
+    case 'A':
+        echo "You did great!";
+        break;
+    case 'B':
+        echo "You did good!";
+        break;
+    case 'C':
+        echo "You did okay.";
+        break;
+    case 'D':
+        echo "You did poorly.";
+        break;
+    case 'F':
+        echo "You failed.";
+        break;
+    default:
+    echo "{$grade} is not a valid grade.";
+    }
+?>
+
 <!DOCTYPE html>
 <html>
     <body>

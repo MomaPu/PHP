816351156e9ac83f998310795368c7046d1c181d
momapu <you@example.com>
2025-12-23 21:44:32 +0300

    texteditor

--- index.php
+++ index.php
@@ -127,6 +127,14 @@ switch ($grade) {
     }
     happyBirthday("SpongeBob", 30);
 ?>
+<?php
+    $username = " Bro Code ";
+    $username = strtolower(trim($username));
+    echo $username; // "bro code"
+    $phone = "123-456-7890";
+    $phone = str_replace("-", "", $phone);
+    echo $phone; // "1234567890"
+?>
 
 <!DOCTYPE html>
 <html>

19d875533658391c357f3747075bab814a69bdec
momapu <you@example.com>
2025-12-23 21:50:13 +0300

    cookie

--- index.php
+++ index.php
@@ -153,6 +153,18 @@ switch ($grade) {
     <p>Welcome to the website.</p>
 <?php include 'footer.html'; ?>
 
+<?php
+    // Установка cookie на 1 день
+    setcookie("favorite_food", "pizza", time() + 86400, "/");
+    // Чтение cookie
+    if (isset($_COOKIE['favorite_food'])) {
+        echo "Buy some {$_COOKIE['favorite_food']}!";
+    }
+    // Удаление cookie (установка времени в прошлом)
+    setcookie("favorite_food", "", time() - 3600, "/");
+?>
+
+
 <!DOCTYPE html>
 <html>
     <body>

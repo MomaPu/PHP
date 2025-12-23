ceb121e787420f7f4c3956d7a3783eb7f6e90271
momapu <you@example.com>
2025-12-23 21:25:29 +0300

    key-value

--- index.php
+++ index.php
@@ -97,7 +97,19 @@ switch ($grade) {
     }
     echo "Number of foods: " . count($foods);
 ?>
-
+<?php
+    $capitals = array(
+        "USA" => "Washington D.C.",
+        "Japan" => "Kyoto",
+        "South Korea" => "Seoul",
+        "India" => "New Delhi"
+    );
+    $capitals["USA"] = "Las Vegas";
+    $capitals["China"] = "Beijing";
+    foreach ($capitals as $key => $value) {
+        echo "{$key} = {$value}<br>";
+    }
+?>
 
 <!DOCTYPE html>
 <html>

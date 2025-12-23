350e58a548ee801b04c59a15412664a3b1be70a8
momapu <you@example.com>
2025-12-23 21:23:19 +0300

    array

--- index.php
+++ index.php
@@ -87,6 +87,17 @@ switch ($grade) {
         }
     }
 ?>
+<?php
+    $foods = array("apple", "orange", "banana", "coconut");
+    $foods[0] = "pineapple";
+    array_push($foods, "kiwi");
+    array_pop($foods);
+    foreach ($foods as $food) {
+        echo $food . "<br>";
+    }
+    echo "Number of foods: " . count($foods);
+?>
+
 
 <!DOCTYPE html>
 <html>

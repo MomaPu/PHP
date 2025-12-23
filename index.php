c1b70d98cb067c5e49445e91022317600e5465c0
momapu <you@example.com>
2025-12-23 21:18:54 +0300

    otschet

--- index.php
+++ index.php
@@ -111,6 +111,18 @@ switch ($grade) {
                 echo "Volume: {$volume} cm³<br>";
             }
         ?>
-
+        <form action="index.php" method="post">
+            <label>Enter a number to count up to:</label><br>
+            <input type="text" name="counter"><br>
+            <input type="submit" value="Start">
+        </form>
+        <?php
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                $counter = $_POST['counter'];
+                for ($i = 1; $i <= $counter; $i++) {
+                    echo $i . "<br>";
+                }
+            }
+        ?>
     </body>
 </html>

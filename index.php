057b5a036d31c45954ab6a24bebe7bf6dc9b3125
momapu <you@example.com>
2025-12-23 21:40:43 +0300

    radio

--- index.php
+++ index.php
@@ -168,5 +168,22 @@ switch ($grade) {
                 }
             }
         ?>
+        <form action="index.php" method="post">
+            <input type="radio" name="credit_card" value="Visa"> Visa<br>
+            <input type="radio" name="credit_card" value="MasterCard"> MasterCard<br>
+            <input type="radio" name="credit_card" value="American Express"> American Express<br>
+            <input type="submit" name="confirm" value="Confirm">
+        </form>
+
+        <?php
+            if (isset($_POST['confirm'])) {
+                $credit_card = $_POST['credit_card'] ?? null;
+                if ($credit_card != null) {
+                    echo "You selected {$credit_card}.";
+                } else {
+                    echo "Please make a selection.";
+                }
+            }
+        ?>
     </body>
 </html>

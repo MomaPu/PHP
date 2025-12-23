a45d2c03c820f8922e3e57beb74bc227819b3023
momapu <you@example.com>
2025-12-23 21:53:10 +0300

    Hash

--- index.php
+++ index.php
@@ -181,6 +181,15 @@ switch ($grade) {
         echo "Form submitted via POST.";
     }
 ?>
+<?php
+    // Динамическое указание файла в action формы
+    $current_file = htmlspecialchars($_SERVER['PHP_SELF']);
+    echo "<form action='{$current_file}' method='post'>";
+    // Проверка метода запроса
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        echo "Form submitted via POST.";
+    }
+?>
 
 
 <!DOCTYPE html>

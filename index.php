1b78049c1f638105a9c602168307a6b2a7fa669c
momapu <you@example.com>
2025-12-23 21:13:30 +0300

    logic operators

--- index.php
+++ index.php
@@ -40,7 +40,15 @@
         echo "You must be 18+ to enter.";
     }
 ?>
-
+<?php
+    $temp = 25;
+    $isSunny = true;
+    if ($temp >= 0 && $temp <= 30 && $isSunny) {
+        echo "The weather is good.";
+    } else {
+        echo "The weather is bad.";
+    }
+?>
 <!DOCTYPE html>
 <html>
     <body>

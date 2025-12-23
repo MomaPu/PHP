b56dd932b989369d6e0ff8117d4e7c48396119a6
momapu <you@example.com>
2025-12-23 21:43:53 +0300

    Function

--- index.php
+++ index.php
@@ -120,6 +120,13 @@ switch ($grade) {
         }
     }
 ?>
+<?php
+    function happyBirthday($first_name, $age) {
+        echo "Happy birthday dear {$first_name}<br>";
+        echo "You are {$age} years old!<br>";
+    }
+    happyBirthday("SpongeBob", 30);
+?>
 
 <!DOCTYPE html>
 <html>

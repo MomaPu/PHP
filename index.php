516599c830122a62dbc078895023e80ff7f92b21
momapu <you@example.com>
2025-12-23 21:06:19 +0300

    add form

--- index.php
+++ index.php
@@ -28,5 +28,20 @@
 <html>
     <body>
         <button>Order a pizza</button>
+        <form action="index.php" method="post">
+            <label>Username:</label><br>
+            <input type="text" name="username"><br>
+            <label>Password:</label><br>
+            <input type="password" name="password"><br>
+            <input type="submit" value="Login">
+        </form>
+        <?php
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                $username = $_POST['username'];
+                $password = $_POST['password'];
+                echo "Username: {$username}<br>";
+            }
+        ?>
+
     </body>
 </html>

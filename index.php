56fcf7c9871e45ad5f313ee772ecaafb5ab046fa
momapu <you@example.com>
2025-12-23 21:42:27 +0300

    radio

--- index.php
+++ index.php
@@ -185,5 +185,22 @@ switch ($grade) {
                 }
             }
         ?>
+        <form action="index.php" method="post">
+            <input type="checkbox" name="foods[]" value="Pizza"> Pizza<br>
+            <input type="checkbox" name="foods[]" value="Hamburger"> Hamburger<br>
+            <input type="checkbox" name="foods[]" value="Taco"> Taco<br>
+            <input type="submit" name="submit" value="Submit">
+        </form>
+
+        <?php
+            if (isset($_POST['submit'])) {
+                if (isset($_POST['foods'])) {
+                    foreach ($_POST['foods'] as $food) {
+                        echo "You like {$food}<br>";
+                    }
+                }
+            }
+        ?>
+
     </body>
 </html>
